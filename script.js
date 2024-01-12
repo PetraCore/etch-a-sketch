75bf68f19c5042e8810a123c7d2b300c90a817db
PetraCore <102828971+PetraCore@users.noreply.github.com>
2024-01-12 15:17:03 +0100

    Generate a grid of pixels into gridbox

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log('Connected.');
+function verifyResolution(resolution) {
+    if (typeof(resolution) !== 'number') {
+        return 16;
+    }
+
+    resolution = Math.abs(resolution);
+    if (resolution > 100) {
+        return 100;
+    }
+    if (resolution < 1) {
+        return 1;
+    }
+
+    return resolution;
+}
+
+function loadPixels(resolution = 16) {
+    resolution = verifyResolution(resolution);
+    const gridbox = document.querySelector('.gridbox');
+    const pxWidth = gridbox.clientWidth / resolution;
+    const pxHeight = gridbox.clientHeight / resolution;
+    const amount = resolution ** 2;
+
+    console.log({pxWidth, pxHeight, amount});
+
+    for (let i = 0; i < amount; i++) {
+        const pixel = document.createElement('div');
+        pixel.style.width = `${pxWidth}px`;
+        pixel.style.height = `${pxHeight}px`;
+        pixel.style.backgroundColor = 'lightblue';
+
+        gridbox.appendChild(pixel);
+    }
+
+}
+
+loadPixels(-10);
